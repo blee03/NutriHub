70a5c486320eb6206ec5e8d2d87e3c42079a8745
alecneps2025 <alecneps@vt.edu>
2024-10-09 15:09:48 -0400

    allows users to name their files

--- main.py
+++ main.py
@@ -15,8 +15,9 @@ def main():
 def result():
     if request.method == 'POST':
         f = request.files['file']
-        filename = secure_filename(f.filename)
-        f.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        filename = request.form.get('fname')
+        extension = os.path.splitext(f.filename)[1]
+        f.save(os.path.join(app.config['UPLOAD_FOLDER'], filename + extension))
         return render_template('index.html', name = f.filename, fileList = os.listdir('uploads'))
 
 if __name__ == '__main__':
